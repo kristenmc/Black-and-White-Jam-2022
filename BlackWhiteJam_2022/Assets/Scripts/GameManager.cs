b1348e0e51daa11d809cff8b861bc0066839b304
Victor Zhou <44218140+Fireryrex@users.noreply.github.com>
2022-04-02 17:57:39 -0700

    Started setting up Game Manager

--- BlackWhiteJam_2022/Assets/Scripts/GameManager.cs
+++ BlackWhiteJam_2022/Assets/Scripts/GameManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] int _numCompleteLoops;
+    [SerializeField] int _currCompleteLoops;
+    [SerializeField] GameObject[] _levelTeleports;
+    [SerializeField] int[] _numKnockTargets;
+    [SerializeField] int _currKnockTargets = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,29 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ProgressLevel()
+    {
+        //Potentially replace this later or add in the level shifts
+        if(_currCompleteLoops <= _levelTeleports.Length)
+        {
+            //Change teleports
+            _levelTeleports[_currCompleteLoops - 1].SetActive(false);
+            _levelTeleports[_currCompleteLoops].SetActive(true);
+            _currCompleteLoops++;
+        }
+
+        //Reset tracking variables
+        _currKnockTargets = 0;
+    }
+
+    public void AddToKnockTarget()
+    {
+        _currKnockTargets++;
+        if(_currKnockTargets >= _numKnockTargets[_currCompleteLoops - 1])
+        {
+            ProgressLevel();
+        }
     }
 }
