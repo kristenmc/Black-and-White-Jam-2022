f1c08941e5a712ecfcd4e1c659c41efb20b80ac6
Victor Zhou <44218140+Fireryrex@users.noreply.github.com>
2022-04-07 18:41:53 -0700

    Added looping and mirror knockables to Level 1

--- BlackWhiteJam_2022/Assets/Scripts/KnockableObject.cs
+++ BlackWhiteJam_2022/Assets/Scripts/KnockableObject.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 public class KnockableObject : MonoBehaviour
 {
     [SerializeField] private GameEvent _knockdownGameEvent;
-    [SerializeField] private GameObject _knockdownMirrorObject;
-    private float _mirrorXDiff;
+    [SerializeField] private GameObject[] _knockdownMirrorObject;
+    private float[] _mirrorXDiff;
     [SerializeField] private Rigidbody2D _knockdownRigidBody;
     [SerializeField] private BoxCollider2D _knockdownCollider;
     [SerializeField] private float _knockdownLaunchForce;
-    private bool _isKnocked = false;
+    private bool _isKnocked = true;
     public bool IsKnocked{get{return _isKnocked;}}
     [SerializeField] bool _randomRotation = false;
+    
     // Start is called before the first frame update
     void Start()
     {
+        _mirrorXDiff = new float[_knockdownMirrorObject.Length];
         if(_knockdownMirrorObject != null)
         {
-            _mirrorXDiff = _knockdownMirrorObject.transform.position.x - transform.position.x;
+            for(int i = 0; i < _knockdownMirrorObject.Length; i++)
+            {
+                _mirrorXDiff[i] = _knockdownMirrorObject[i].transform.position.x - transform.position.x;
+            }
         }
         if(_randomRotation)
         {
             Quaternion rotation = transform.localRotation;
             rotation.eulerAngles = new Vector3(rotation.eulerAngles.x, rotation.eulerAngles.y, Random.Range(-30, 30));
             transform.localRotation = rotation;
+            AlignMirrors();
+            _isKnocked = false;
         }
         
     }
@@ -32,13 +39,22 @@ public class KnockableObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_knockdownMirrorObject != null && _isKnocked)
-        {
-            _knockdownMirrorObject.transform.position = new Vector3(transform.position.x +_mirrorXDiff, transform.position.y, transform.position.z);
-            _knockdownMirrorObject.transform.rotation = transform.rotation;
-        }
+        AlignMirrors();
     }
 
+    public void AlignMirrors()
+    {
+        if(_knockdownMirrorObject != null && _isKnocked)
+        {
+            for(int i = 0; i < _knockdownMirrorObject.Length; i++)
+            {
+                _knockdownMirrorObject[i].transform.position = new Vector3(transform.position.x +_mirrorXDiff[i], transform.position.y, transform.position.z);
+                _knockdownMirrorObject[i].transform.rotation = transform.rotation;
+            }
+
+        }
+    }
+    
     public void KnockDownObject()
     {
         _knockdownGameEvent.Invoke();
