f501034e203883b2f9d2ed16214b84d08045457d
Molina <molinakmey@gmail.com>
2022-04-08 15:39:25 -0700

    Optimize level-editting workflow

--- BlackWhiteJam_2022/Assets/Scripts/KnockableObject.cs
+++ BlackWhiteJam_2022/Assets/Scripts/KnockableObject.cs
@@ -2,69 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnockableObject : MonoBehaviour
+public class KnockableObject : MirroredObject
 {
     [SerializeField] private GameEvent _knockdownGameEvent;
-    [SerializeField] private GameObject[] _knockdownMirrorObject;
-    private float[] _mirrorXDiff;
     [SerializeField] private Rigidbody2D _knockdownRigidBody;
     [SerializeField] private BoxCollider2D _knockdownCollider;
     [SerializeField] private float _knockdownLaunchForce;
     private bool _isKnocked = true;
     public bool IsKnocked{get{return _isKnocked;}}
     [SerializeField] bool _randomRotation = false;
+    [SerializeField] bool _ragDollPhysics = true;
     
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        _mirrorXDiff = new float[_knockdownMirrorObject.Length];
-        if(_knockdownMirrorObject != null)
-        {
-            for(int i = 0; i < _knockdownMirrorObject.Length; i++)
-            {
-                _mirrorXDiff[i] = _knockdownMirrorObject[i].transform.position.x - transform.position.x;
-            }
-        }
+        base.Start();
+        _isKnocked = false;
+        
+    }
+
+    protected override void InitialSetup()
+    {
+        base.InitialSetup();
         if(_randomRotation)
         {
             Quaternion rotation = transform.localRotation;
             rotation.eulerAngles = new Vector3(rotation.eulerAngles.x, rotation.eulerAngles.y, Random.Range(-30, 30));
             transform.localRotation = rotation;
         }
-        AlignMirrors();
-        _isKnocked = false;
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         AlignMirrors();
     }
 
-    public void AlignMirrors()
+    public override void AlignMirrors()
     {
-        if(_knockdownMirrorObject != null && _isKnocked)
+        if(_isKnocked)
         {
-            for(int i = 0; i < _knockdownMirrorObject.Length; i++)
-            {
-                _knockdownMirrorObject[i].transform.position = new Vector3(transform.position.x +_mirrorXDiff[i], transform.position.y, transform.position.z);
-                _knockdownMirrorObject[i].transform.rotation = transform.rotation;
-            }
-
+            base.AlignMirrors();
         }
+        
     }
+
     
     public void KnockDownObject()
     {
         _knockdownGameEvent.Invoke();
         //Start the knockdown object animation here
-        _knockdownRigidBody.constraints = RigidbodyConstraints2D.None;
-        _knockdownRigidBody.AddForce(transform.up * _knockdownLaunchForce);
-        _knockdownRigidBody.AddTorque(180);
+        if(_ragDollPhysics)
+        {
+            _knockdownRigidBody.constraints = RigidbodyConstraints2D.None;
+            _knockdownRigidBody.AddForce(transform.up * _knockdownLaunchForce);
+            _knockdownRigidBody.AddTorque(180);
+        }
         _isKnocked = true;
         _knockdownCollider.isTrigger = true;
         //Then probably destroy or hide the object
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
---
+++ BlackWhiteJam_2022/Assets/Scripts/MirroredObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirroredObject : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _mirrorObject;
+    [SerializeField] private float _unitWidth = 1;
+    private float[] _mirrorXDiff;
+    static float _levelLoopDistance = 18;
+    protected virtual void Start()
+    {
+        InitialSetup();
+        AlignMirrors();
+        
+    }
+
+    protected virtual void InitialSetup()
+    {
+        _mirrorXDiff = new float[_mirrorObject.Length];
+        if(_mirrorObject != null)
+        {
+            for(int i = 0; i < _mirrorObject.Length; i++)
+            {
+                _mirrorXDiff[i] = _mirrorObject[i].transform.position.z * (_levelLoopDistance - _unitWidth);
+            }
+        }
+    }
+
+    public virtual void AlignMirrors()
+    {
+        if(_mirrorObject != null)
+        {
+            for(int i = 0; i < _mirrorObject.Length; i++)
+            {
+                _mirrorObject[i].transform.position = new Vector3(transform.position.x +_mirrorXDiff[i], transform.position.y, transform.position.z);
+                _mirrorObject[i].transform.rotation = transform.rotation;
+            }
+
+        }
+    }
+
+}
