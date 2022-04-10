7826fc8d8b910379fc93d263a32f4059a46cb2a8
Victor Zhou <44218140+Fireryrex@users.noreply.github.com>
2022-04-09 17:27:19 -0700

    Implemented new ice cream truck sprite and wheel rotation

--- BlackWhiteJam_2022/Assets/Scripts/IceCreamTruck/IceCreamBackgroundLooper.cs
+++ BlackWhiteJam_2022/Assets/Scripts/IceCreamTruck/IceCreamBackgroundLooper.cs
@@ -18,7 +18,7 @@ public class IceCreamBackgroundLooper : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(_isLooping)
         {
